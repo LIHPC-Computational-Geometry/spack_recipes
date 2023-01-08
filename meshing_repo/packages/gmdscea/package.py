b71c4b4b47f863cbdfd9010b8c2caf094739a416
franck.ledoux <franck.ledoux@cea.fr>
2023-01-08 17:59:30 +0100

    add gmdscea recipe updated for github integration

---
+++ meshing_repo/packages/gmdscea/package.py
@@ -0,0 +1,28 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Gmdscea(CMakePackage):
+
+    homepage = 'https://github.com/LIHPC-Computational-Geometry/gmdsceal'
+    url = 'https://github.com/LIHPC-Computational-Geometry/gmdscea/archive/refs/tags/v2.0.0.tar.gz'
+    git = 'https://github.com/LIHPC-Computational-Geometry/gmdscea.git' 
+    maintainers = ['meshing_team']
+
+    version('main', branch='main')
+    version('2.0.0')#, sha256='98c018d9c4e44364332a69fe0de70023c7fc09f39c259cf59ec87d8244a8626e')
+
+    variant('shared', default=True, description='Build as a shared library.')
+
+    depends_on('mgx_gmds')
+    depends_on('lima')
+
+    def cmake_args(self):
+        args = []
+        args.append(self.define_from_variant('BUILD_SHARED_LIBS', 'shared'))
+
+        return args
