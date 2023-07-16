f583ca942121eaa71825cfa1060f5c6989bbccd5
Benoit Lelandais <41573600+lelandaisb@users.noreply.github.com>
2023-07-16 14:30:55 +0200

    Version realization improved

--- .github/workflows/list-versions.py
+++ .github/workflows/list-versions.py
@@ -3,6 +3,9 @@ import re
 import sys
 import os
 
+# Parse the list of recipes and create a markdown file
+# containing each product name and associated last version.
+# Used in CI when a new is release is creted to comment it.
 def listVersions(source_path):
     recipes = glob.glob(source_path + '/**/package.py', recursive=True)
     with open(source_path + '/recipes.md', 'w') as md:
@@ -16,8 +19,7 @@ def listVersions(source_path):
                 version = re.search("version\([\'\"]([0-9]+(\.[0-9]+)*)[\'\"]", line)
                 version_name = 'undefined' if version is None else version.group(1)
                 md.write('| ' + product_name + ' | ' + version_name + ' |\n')
-
             
 if __name__ == "__main__":
-        project_path = sys.argv[1]
-        listVersions(project_path)
+    project_path = sys.argv[1]
+    listVersions(project_path)
