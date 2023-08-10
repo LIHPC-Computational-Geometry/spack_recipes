036a57d1a5d54c52853763ef30aa85d4d4361c5e
Benoit LELANDAIS <benoit.lelandais@cea.fr>
2023-08-10 15:00:33 +0200

    Minor bug fixes in recipes

--- meshing/packages/qtutil/package.py
+++ meshing/packages/qtutil/package.py
@@ -12,7 +12,7 @@ class Qtutil(CMakePackage):
     """Bibliotheque d'utilitaires Qt"""
 
     homepage = 'https://github.com/LIHPC-Computational-Geometry/qtutil'
-    url = 'https://github.com/LIHPC-Computational-Geometry/qtutil/archive/refs/tags/5.7.8.tar.gz'
+    url = 'https://github.com/LIHPC-Computational-Geometry/qtutil/archive/refs/tags/6.2.0.tar.gz'
     git = 'https://github.com/LIHPC-Computational-Geometry/qtutil.git'
     maintainers = ['meshing_team']
 
