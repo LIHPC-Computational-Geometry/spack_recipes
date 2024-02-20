8e22875eb74c4be4ba8632125d8568773f0318d9
nicolas le goff <nicolas.le-goff@cea.fr>
2024-02-20 21:23:11 +0100

    added new project machine_types and removed internal machine_types from smooth3d

--- meshing/packages/smooth3d/package.py
+++ meshing/packages/smooth3d/package.py
@@ -19,6 +19,7 @@ class Smooth3d(CMakePackage):
     git = 'https://github.com/LIHPC-Computational-Geometry/smooth3d.git'
     maintainers = ['meshing_team']
 
+    version('3.3.0', sha256='2b392a2a4ed7b58ea503b423335369edf727e7c8e8054b2a321d4a2160b95a64')
     version('3.2.1', sha256='e26fde58bebdf64134ae52fb6d1d1e95490c725a479370038c3093a46de4c837')
     version('3.2.0', sha256='5e0132a5a6c75d18247db1fccfd7d64cdca7d9a8bb44c246616527a92fe809ea')
     version('3.0.2')#, sha256='0be62f149fa154621dbdeefdf00d026bb2bd26ff7bd88c8560bce652ff209038')
@@ -26,16 +27,14 @@ class Smooth3d(CMakePackage):
     version('3.0.0', sha256='9ef4b2e380c507cc14883a6233c1df933bf3fc49549fd7e3f87630ae2abb6c82')
 
     variant('shared', default=True, description='Build as a shared library.')
-    variant('machinetypes', default=False, description='Links with an external machinetypes.')
 
     depends_on('gmds')
     depends_on('mesquite')
     depends_on('lima')
-    depends_on('machine-types', when='+machinetypes')
+    depends_on('machine-types')
 
     def cmake_args(self):
         args = []
         args.append(self.define_from_variant('BUILD_SHARED_LIBS', 'shared'))
-        args.append(self.define_from_variant('WITH_EXTERNAL_MACHINETYPES', 'machinetypes'))
 
         return args
