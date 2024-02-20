22dac91773e7cf133fab3b240eb03764236612b0
nicolas le goff <nicolas.le-goff@cea.fr>
2024-02-20 23:14:11 +0100

    modify the build script with more explanations

--- build_spack.sh
+++ build_spack.sh
@@ -5,16 +5,35 @@ git clone --depth=1 -b v0.20.1  https://github.com/spack/spack.git
 # can be mandatory if you have already used spack on your computer
 # delete the .spack directory in the home of the user 
 #==========================================
+# our recipes
 git clone https://github.com/LIHPC-Computational-Geometry/spack_recipes.git
+#==========================================
+# hard-coded modifications to spack configuration
 
 # this is used to declare opengl as non buildable and use the system library 
 cp ./spack_recipes/config/packages.yaml ./spack/etc/spack/
 
+# to register our recipes; it assumes that spack_recipes and spack are located at
+# the same level. You can use the "spack repo add" commands instead of copying the repos.yaml file
+#spack repo add ./spack_recipes/meshing
+#spack repo add ./spack_recipes/meshing_supersede
+cp spack_recipes/config/repos.yaml spack/etc/spack/defaults/repos.yaml
+
+#==========================================
+# configure spack; it modifies the .spack directory in the user home
 source ./spack/share/spack/setup-env.sh
-spack repo add ./spack_recipes/meshing
-spack repo add ./spack_recipes/meshing_supersede
-spack compiler find
+spack clean -a
 spack external find cmake
+
+spack compiler find
+# spack uses the highest version of the compiler found by default; if it is incomplete,
+# for example the C compiler is installed but not the CXX one the installations will fail.
+# Compilers found can be investigated by (see `spack help compiler` commands)
+#spack compiler list
+#spack compiler info gcc
+# An undesirable version can be removed by editing ~/.spack/linux/compilers.yaml or using
+#spack compiler remove gcc@12
+
 #==========================================
 
 # +mpi should actually be ok, but currently the default openmpi install fails
@@ -24,6 +43,10 @@ spack spec magix3d ^vtk-maillage~opengl2+qt~mpi ^hdf5~mpi ^cgns~mpi ^mesquite~mp
 # to work on the main branch
 git clone git@github.com:LIHPC-Computational-Geometry/magix3d.git
 spack dev-build -d ./magix3d magix3d@2.2.7 ^vtk-maillage~opengl2+qt~mpi ^hdf5~mpi ^cgns~mpi ^mesquite~mpi
+
+# to configure an IDE
+# get the CMAKE_PREFIX_PATH with ';' as separators
+#spack load --only dependencies --sh magix3d | grep CMAKE_PREFIX_PATH | awk -F "=" {'print $2'} | sed 's/:/;/g'
 
 #==========================================
 # testing the install
