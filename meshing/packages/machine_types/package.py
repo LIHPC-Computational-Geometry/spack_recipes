3fbc8139d94d4a82f01ab1f7eb58be3ceafe7118
Benoit Lelandais <41573600+lelandaisb@users.noreply.github.com>
2024-02-27 13:52:08 +0100

    Update package.py

--- meshing/packages/machine_types/package.py
+++ meshing/packages/machine_types/package.py
@@ -14,4 +14,4 @@ class MachineTypes(CMakePackage):
     url = 'https://github.com/LIHPC-Computational-Geometry/machine_types/archive/refs/tags/0.0.0.tar.gz'
     git = 'https://github.com/LIHPC-Computational-Geometry/machine_types.git'
 
-    version('2.0.0', sha256="15f2c41531d8ee70c45ca98a9ab1e1d1845403d75be4e2a7aa2b3335ade2d922")
+    version('2.0.0', sha256='fb7d0095b2b4028e1cead97363c3e6c0bdaaa582ae6e90cd7f70cb8e344a730e')
