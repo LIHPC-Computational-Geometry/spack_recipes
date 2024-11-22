01d2348ef85494ab0106ee452c67851465ce8e0c
CharlesPignerol <121161149+CharlesPignerol@users.noreply.github.com>
2024-11-22 10:41:14 +0100

    QtVtk 8.6.0 recipe.
    By default, all prints are made in soft rendering. 
    Removed spack dependency on Xerces-C.

--- meshing/packages/qtvtk/package.py
+++ meshing/packages/qtvtk/package.py
@@ -21,13 +21,13 @@ class Qtvtk(CMakePackage):
     depends_on('qtutil@5: ~shared', type=('build', 'link'), when='~shared')
     depends_on('vtkcontrib@4: +shared', type=('build', 'link'), when='+shared')
     depends_on('vtkcontrib@4: ~shared', type=('build', 'link'), when='~shared')
-    depends_on('xerces-c', type=('build', 'link'))
     depends_on('preferences', type=('build', 'link'))
     depends_on('qt')
     depends_on('vtk-maillage +qt', type=('build', 'link'))
 
     variant('shared', default=True, description='Creation de bibliotheques dynamiques (defaut:shared, annuler le defaut par ~shared)')
 
+    version('8.6.0', sha256='95968e9fb03e6fe229efbbc70056a4e3184d4bb6879abdbeb7e84fc17695d869')
     version('8.5.1', sha256='6a9ee744bde29ecdd157c23aff6036e614b03907dc05a6a3098fa1465e7360bc')
     version('8.5.0', sha256='134ecd42a13e2409f3cbc9a634ec8d1ab0ed763cdb9c6c05a4ca72586af8b0b7')
     version('8.4.1', sha256='8b5b0f74a27fd0664b4c94229d8488fb62fa34b2fd7a7367085215a2f850f786')
