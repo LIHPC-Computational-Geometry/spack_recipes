557dc23334ebbd59fceb8762278cef29da72fea0
nicolas le goff <nicolas.le-goff@cea.fr>
2026-02-13 13:11:36 +0100

    magix3d-2.6.3

--- meshing/packages/magix3d/package.py
+++ meshing/packages/magix3d/package.py
@@ -20,6 +20,7 @@ class Magix3d(CMakePackage):
     variant('pythonaddon', default=False, description='Additional python modules to enrich PYTHONPATH')
     variant('doc', default=False, description='Installation de la documentation utilisateur')
 
+    version('2.6.3', sha256='b875c0e08b9f3779322338bb436b1865ee453d5c77a5ea27300a87cf03d1d757')
     version('2.6.2', sha256='51f15612423f795b5983e893e0c527ae2a82254e0a7beef189959caca6931457')
     version('2.6.1', sha256='3a679d56b332691f2cf0e0ff609e8f67596b4badf7db1f9e1a464bfc4a2c889d')
     version('2.6.0', sha256='ea1aac98f9f32c83b04af027609f424dbc31a6a92e3c1b023478d3344c2b8dae')
