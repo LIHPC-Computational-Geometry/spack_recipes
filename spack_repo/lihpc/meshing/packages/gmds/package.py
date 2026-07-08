028674d57c9e667f6de1a0b10173fe6fad3154b1
Pierre Duperray <pierre.duperray@cea.fr>
2026-07-08 15:57:03 +0200

    fixed header in gmds recipes
    spack audit packages complained about it

--- spack_repo/lihpc/meshing/packages/gmds/package.py
+++ spack_repo/lihpc/meshing/packages/gmds/package.py
@@ -1,24 +1,9 @@
-# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
+# -*- coding: utf-8 -*-
+##############################################################################
+# Project Gmds
 #
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install gmds
-#
-# You can edit this file again by typing:
-#
-#     spack edit gmds
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
+# CEA/DAM/DSSI, 2020
+##############################################################################
 
 from spack.package import *
 from spack_repo.builtin.build_systems.cmake import CMakePackage
