a3262a2d01d64e9b86d8e3eb0c6e9d43be66377e
CharlesPignerol <121161149+CharlesPignerol@users.noreply.github.com>
2026-09-02 07:40:15 +0200

    Recipe of QQualif v 4.7.0
    Use of the separate_debug_infos function from GUIToolkitsVariables.

--- meshing/packages/qqualif/package.py
+++ meshing/packages/qqualif/package.py
@@ -38,6 +38,7 @@ class Qqualif(CMakePackage):
     variant("gmds", default=True, description="Utilisation de la structure de maillages GMDS")
     variant("vtk", default=True, description="Utilisation de la structure de maillages VTK")
 
+    version("4.7.0", sha256="07bb15e00afe0df435d9b21957091b43d2ac1c52931e94db8856b86b54eae986")
     version("4.6.0", sha256="78fbbbd4919b5b6a96ee684e66baafa074c2a36328829ac120625e1a6540da14")
     version("4.5.0", sha256="ae16c1e0c44a5ad3843e9bc7e6beca3d800ce0c887faf646e06b8f0dfb40363e")
     version("4.4.0", sha256="0d8d315c1131ec36af92b6455880c4071ef3b06c36bebca55075c94b3e347224")
